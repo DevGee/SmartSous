4bad2fe128157cb19afdb8b08894e993ca1c8a41
Steven Shih <stevenshih1997@gmail.com>
2017-10-01 11:54:57 -0700

    Test Linting

--- client/App.js
+++ client/App.js
@@ -11,12 +11,17 @@ const styles = StyleSheet.create({
 });
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.name = 'Steven';
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{this.name}</Text>
       </View>
     );
   }
