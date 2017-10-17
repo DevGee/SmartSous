2f205e8a3eab89f2dda90e4c23192dc0d8f34172
andrewmlaw <andrewmlaw@gmail.com>
2017-10-17 12:14:31 -0700

    basic communication with db working

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,25 @@ import psycopg2
 
 app = Flask(__name__)
 
+def dbQuery():
+    try:
+        conn = psycopg2.connect("dbname='smartsous' user='ss' password='devg' host='localhost'")
+        print("connected okay")
+    except:
+        print ("Unable to connect to the database")
+        
+    try:
+        cur = conn.cursor()
+        cur.execute("select rec_name, cook_time from recipe inner join fridge_recipe on recipe.rec_id = fridge_recipe.rec_id inner join comm on comm.fr_id = fridge_recipe.fr_id where comm.fr_id = 1;")
+        rows = cur.fetchall()
+        #print (rows)
+        return str(rows)
+    except:
+        print ("cannot select")
+
+
+
+
 @app.route('/')
 def hello_world():
     """docstring for hello_world"""
@@ -11,33 +30,7 @@ def hello_world():
 
 @app.route('/api/test', methods=['GET'])
 def get_string():
-    dbQuery()
-    return 'testing api GET'
-
-
-def dbQuery():
-    try:
-        conn = psycopg2.connect("dbname='smartsous' user='postgres' host='localhost'")
-        print("connected okay")
-
-    except:
-        print ("Unable to connect to the database")
-        
-    cur = conn.cursor()
-    try:
-        cur.execute("select rec_name, cook_time from recipe inner join fridge_recipe on recipe.rec_id = fridge_recipe.rec_id inner join comm on comm.fr_id = fridge_recipe.fr_id where comm.fr_id = 1;")
-    except:
-        print ("cannot select")
-
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print ("  ", row[0])
-
-
-
-
-
+    #dbQuery()
+    return dbQuery()
 
 
