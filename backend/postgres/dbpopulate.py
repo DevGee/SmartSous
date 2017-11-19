0642a304b41558a908c52673682f3527d11c34a6
andrewmlaw <andrewmlaw@gmail.com>
2017-11-19 02:20:32 +0000

    script for populating recipes in database

--- backend/postgres/dbpopulate.py
+++ backend/postgres/dbpopulate.py
@@ -8,15 +8,27 @@ def dbPopulate():
         cur = conn.cursor()
         #print("connected okay")
     except psycopg2.Error as e:
-        #print ("Unable to connect to the database")
-        #print (e.pgerror)
+        print ("Unable to connect to the database")
+        print (e.pgerror)
         return e.pgerror
 
-    with open('recipe-data-test.json') as outfile:
+    with open('../../scraper/recipe-data-test.json') as outfile:
         data = json.load(outfile)
         outfile.close() 
 
-    print(data)
+    #print(data)
+   
+    for recipe in data:
+        #cur.execute("""insert into recipe (rec_name, cook_time, ingred, instr, pic_url, servings) values (%s, %s, %s, %s, %s, %s);""", (data[0]['title'], data[0]['time'], data[0]['ingred'], data[0]['instr'], data[0]['picurl'], data[0]['servings']))
+        #cur.execute("""insert into recipe (rec_name, cook_time, ingred, instr, pic_url, servings) values (%s, %s, %s, %s, %s, %s);""", (recipe['title'], recipe['time'], recipe['ingred'], recipe['instr'], recipe['picurl'], recipe['servings']))
+
+    conn.commit()
+    
+    
+    
+    #print(query_str.format(data[0]['title'], data[0]['time'], data[0]['ingred'], data[0]['instr'], data[0]['picurl'], data[0]['servings']))
+        #print(recipe['title'])
+
 
 
 if __name__ == "__main__":
